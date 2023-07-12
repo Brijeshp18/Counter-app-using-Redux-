e14980a3304058ba08bb29e7fdaba3735057ed3f
Brijeshp18 <brijeshbri18@gmail.com>
2023-07-12 22:04:17 +0530

    counter app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import Counter from './components/Counter'
+import React from "react";
+import Counter from "./components/Counter";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-     <Counter/>
-
-
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Counter />} />
+          <Route path="/Counter-app-using-Redux-" element={<Counter />} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
